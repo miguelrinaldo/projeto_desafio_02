fac088c837ebba82effbeb478d766def8b4ac79e
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 20:05:52 -0300

    Demonstrado criação de variáveis

---
+++ js/script_fundamentos.js
@@ -0,0 +1,41 @@
+//Comentário de uma linha 
+
+/*
+Comentário de múltiplas linhas 
+É possível ter várias 
+linhas
+*/
+
+//COMANDO DE SAÍDA
+console.log("Olá mundo!!")
+
+//DECLARAÇÃO DE VARIÁVEL
+let num = 10 
+var num2 = 50
+const num3 = 100
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 250
+console.log(num4)
+
+var num5 = 140
+}
+
+console.log(num5)
+
+num=1882
+
+console.log(num)
+
+num2 = 58
+
+console.log(num2)
+
+num3 = 77
+
+console.log(num3)
+
